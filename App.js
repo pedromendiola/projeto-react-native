7b21886e447361619364fc53a2be7a198f1e9508
Pedro Mendiola <pedro.enrrique29@outlook.com>
2022-04-10 23:53:19 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback,useState, useEffect} from 'react'
+import 'react-native-gesture-handler'
+import { Platform } from 'react-native'
+import { NavigationContainer } 
+       from '@react-navigation/native'
+import { createDrawerNavigator } 
+       from '@react-navigation/drawer'
+import { Ionicons } from '@expo/vector-icons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import Tela from './src/screens/Tela'
+
+import InicioScreen from './src/screens/Inicio'
+import ConfiguracoesScreen from './src/screens/Configuracoes'
+import GameScreen from './src/screens/Jogo'
+
+const Drawer = createDrawerNavigator()
+
+export default function App(){
+  
+  const [iconePadrao, setIconePadrao] = useState('md')
+  
+  useEffect(()=>{
+    
+    //Dependendo do SO, mostraremos ícones diferentes
+    switch(Platform.OS){
+      case 'ios':
+        setIconePadrao('ios')
+        break
+      case 'android':
+        setIconePadrao('md')
+        break
+    }
+  },[])
+
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName='Início'
+        screenOptions={{
+          drawerStyle: { backgroundColor: '#FFF',
+        width: 280}
+        }}>
+          <Drawer.Screen name="Início" 
+          component={InicioScreen}
+          options={{
+          drawerIcon: ({ focused }) => (            
+              <Ionicons name={`${iconePadrao}-home`}
+               size={32}
+               color={focused ? '#7CC' : '#CCC'} />)
+          }}
+          /> 
+          <Drawer.Screen name="Configurações" 
+          component={ConfiguracoesScreen} 
+          options={{
+            drawerIcon: ({ focused }) => (            
+                <Ionicons name={`${iconePadrao}-cog`}
+                 size={32}
+                 color={focused ? '#7CC' : '#CCC'} />)
+            }}
+          /> 
+          <Drawer.Screen name="Game" 
+          component={GameScreen} 
+          options={{
+            drawerIcon: ({ focused }) => (            
+                <Ionicons name={`${iconePadrao}-game-controller`}
+                 size={32}
+                 color={focused ? '#7CC' : '#CCC'} />)
+            }}
+          />         
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Tela.jsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+function Tela(){
+    const [appAtivo, setAppAtivo] = useState(false)
+  
+    useEffect(()=>{
+    
+    // codigo da splash screen
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Ionicons.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience.
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppAtivo(true);
+      }
+    }
+    //fim codigo da splash screen
+    prepare();
+  },[])
+
+  // codigo da splash screen 
+  const onLayoutRootView = useCallback(async () => {
+    if (appAtivo) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appAtivo]);
+
+  if (!appAtivo) {
+    return null;
+  }
+ //fim codigo da splash screen
+
+ return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+      <Entypo name="rocket" size={30} />
+    </View>
+  );
+
+}
+export default Tela
